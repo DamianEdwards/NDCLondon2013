6ceed8789d6a67e3530073935deef4dcaa69d461
DamianEdwards <damian@damianedwards.com>
2013-12-04 16:14:05 +0000

    Added Auth demo

---
+++ Auth/AuthHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace WebApplication22
+{
+    public class AuthHub : Hub
+    {
+        public override Task OnConnected()
+        {
+            var userName = Context.User.Identity.Name;
+
+            var msg = "Hello " + userName + ".";
+            if (Context.User.IsInRole("Admin"))
+            {
+                msg += " You are an Admin!";
+            }
+            Clients.User(userName).helloWorld(msg);
+
+            return base.OnConnected();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public void DoSecretStuff()
+        {
+
+        }
+    }
+}
---
+++ Auth/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApplication22.Startup))]
+
+namespace WebApplication22
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            app.Map("/login", map =>
+            {
+                map.Run(async context =>
+                {
+                    var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationType);
+                    var username = context.Request.Query["username"] ?? "[random]";
+                    identity.AddClaim(new Claim(ClaimTypes.Name, username));
+                    if (String.Equals(username, "Damian", StringComparison.OrdinalIgnoreCase))
+                    {
+                        identity.AddClaim(new Claim(ClaimTypes.Role, "Admin", ClaimValueTypes.String));
+                    }
+                    context.Authentication.SignIn(identity);
+                });
+            });
+
+            app.MapSignalR(new HubConfiguration { EnableDetailedErrors = true });
+        }
+    }
+}
