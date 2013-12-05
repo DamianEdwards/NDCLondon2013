b1299438aaca70232202b06a2a0ae2e49f525ec0
DamianEdwards <damian@damianedwards.com>
2013-12-05 13:52:02 +0000

    Added send from back-end system demo

---
+++ BackEndSystem/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BackEndSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "http://localhost:46246/backend";
+
+            using (var http = new HttpClient())
+            {
+                while (true)
+                {
+                    var message = new
+                    {
+                        message = String.Format("Time from back-end system: {0}", DateTimeOffset.Now)
+                    };
+
+                    http.PostAsync(url, new StringContent(JsonConvert.SerializeObject(message))).Wait();
+
+                    Console.WriteLine("Sent message {0}", message);
+
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
---
+++ SendFromBackEndSystem/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Newtonsoft.Json;
+using Owin;
+
+[assembly: OwinStartup(typeof(SendFromBackEndSystem.Startup))]
+
+namespace SendFromBackEndSystem
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map("/backend", map =>
+            {
+                map.Run(async context =>
+                {
+                    string body;
+                    using (var reader = new StreamReader(context.Request.Body))
+                    {
+                        body = await reader.ReadToEndAsync();
+                    }
+                    dynamic payload = JsonConvert.DeserializeObject(body);
+                    string message = payload.message;
+                    
+                    var hub = GlobalHost.ConnectionManager.GetHubContext<MessageHub>();
+                    hub.Clients.All.newMessage(message);
+                });
+            });
+
+            app.MapSignalR();
+        }
+    }
+}
